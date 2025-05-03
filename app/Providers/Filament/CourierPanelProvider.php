50fdfe5a7b375ed85234c2c6d4472db5bf4e8937
mulil abror <root087732168347@gmail.com>
2025-05-03 20:40:11 +0700

    fix middleware

--- app/Providers/Filament/CourierPanelProvider.php
+++ app/Providers/Filament/CourierPanelProvider.php
@@ -17,7 +17,6 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use App\Http\Middleware\RedirectToProperPanelMiddleware;
 
 class CourierPanelProvider extends PanelProvider
 {
